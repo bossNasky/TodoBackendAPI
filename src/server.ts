91cd4115dab43321823cf6ea0db33e3f23827a24
bossNasky <naskret.kamil@wp.pl>
2023-03-10 18:57:52 +0100

    Install packages, init server
    Install express and dotenv packages, init express server.

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Someone");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./src/config/.env" });
+
+const application = express();
+
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
+
+application.listen(SERVER_PORT, () => {
+  console.log(`Server is listening at port ${SERVER_PORT}...`);
+});
