394319614ce20905f238c7273f4c22685e2b6473
bossNasky <naskret.kamil@wp.pl>
2023-03-11 13:44:51 +0100

    Add a cors
    Add cors package and initialize with express.

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import {
   notFound,
 } from "./controllers/error-controller";
 import { todoRouter } from "./router/todo-router";
+import cors from "cors";
 
 const application = express();
 
 application.use(express.json());
+application.use(cors());
 application.use("/api/v1/todos", todoRouter);
 application.all("*", notFound);
 application.use(globalErrorMiddleware);
